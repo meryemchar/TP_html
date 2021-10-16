945d9eac19f87731d4ccf9664ed45a5de41075b0
Meryem Charit <charitmeryem@gmail.com>
2021-10-16 19:21:01 +0200

    Affichage progressif

--- scriptsCV.js
+++ scriptsCV.js
@@ -5,7 +5,29 @@ function myFunction(id) {
     } else {
       x.style.display = "none";
 }
+
+  
+// setInterval(function(){ alert("intervale"); }, 3);
   }
+
+  function ApparitionProgressive(id)
+  {
+      var el = document.getElementById(id);
+        var style = window.getComputedStyle(el).getPropertyValue('font-size');
+        var fontSize1 = parseFloat(style); 
+        el.style.fontSize=3+'px';
+        el.style.display="block";
+        intervalId= setInterval(function(){
+                                            var fontSize=parseFloat(window.getComputedStyle(el).getPropertyValue('font-size'));
+                                            el.style.fontSize=(fontSize+0.5)+'px';       
+                                            if(fontSize===fontSize1-1)
+                                              {
+                                                clearInterval(intervalId);
+                                              }             
+                                          },200);     
+  } 
+  
+
 
   function myFunction2(id1,classe) {
     // var x1 = document.getElementById(id1);
@@ -15,10 +37,12 @@ function myFunction(id) {
         {x2[i].style.display="none";
             if(x2[i].getAttribute('id')===id1 )
                 {
-                    x2[i].style.display = "block";
+                    //x2[i].style.display = "block";
+                    ApparitionProgressive(id1);
                 } 
         }
     //x1.style.display = "block";
-    
   }
+
+  
 
